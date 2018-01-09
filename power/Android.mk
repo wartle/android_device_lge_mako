fa61ebc640535ab29a999fb195fddacf4c190f9d
Daniel Jarai <jaraidaniel@gmail.com>
2018-01-09 22:21:25 +0100

    mako: Convert to binderized HAL 1.0
    Mostly based on the wahoo power HAL.
    
    Change-Id: Ibd47833e77475df155c7f2d88f37f9ec04817c13
    Signed-off-by: Dániel Járai <jaraidaniel@gmail.com>

--- power/Android.mk
+++ power/Android.mk
@@ -1,21 +1,41 @@
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(TARGET_BOARD_AUTO),true)
-  ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
-    LOCAL_PATH := $(call my-dir)
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-    # HAL module implemenation stored in
-    # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
-    include $(CLEAR_VARS)
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
 
-    LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-    LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-    LOCAL_SRC_FILES := \
-                       power.c \
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_OWNER := qcom
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -Wno-unused-parameter
 
-    LOCAL_MODULE:= power.$(TARGET_BOARD_PLATFORM)
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_CFLAGS += -Wno-unused-parameter
-    include $(BUILD_SHARED_LIBRARY)
-  endif
-endif
+LOCAL_MODULE := android.hardware.power@1.0-service.mako
+LOCAL_INIT_RC := android.hardware.power@1.0-service.mako.rc
+LOCAL_SRC_FILES := service.cpp \
+                   Power.cpp \
+                   power-helper.c
+
+LOCAL_SHARED_LIBRARIES := liblog \
+                          libcutils \
+                          libhidlbase \
+                          libhidltransport \
+                          libhardware \
+                          libutils \
+                          android.hardware.power@1.0
+
+include $(BUILD_EXECUTABLE)
