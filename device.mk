67015b7c57743919ccaf42bf502ed0f673d9ee1a
Daniel Jarai <jaraidaniel@gmail.com>
2018-01-15 00:01:18 +0100

    mako: Binderize GNSS HAL
    Change-Id: I547f698cc3abb6770eb601bb9efb276cb38eb48a

--- device.mk
+++ device.mk
@@ -126,7 +126,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
