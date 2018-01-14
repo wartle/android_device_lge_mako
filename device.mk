3b0e2a143fde721220c8cd23e41180ea8415ca01
Daniel Jarai <jaraidaniel@gmail.com>
2018-01-15 00:07:40 +0100

    mako: Binderize memtrack HAL
    Change-Id: Ia129ac71bb6d8ad9be99c66106e9659bde42c2a5

--- device.mk
+++ device.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     libgenlock \
     liboverlay \
     hwcomposer.msm8960 \
