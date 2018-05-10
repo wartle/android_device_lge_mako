a2b8912f6bbdd2c0c8ba9ad3aa990e6d21f8a186
Sudokamikaze <sudokamikaze@protonmail.com>
2018-05-10 15:53:16 +0300

    mako: Binderize vibrator service
    Change-Id: Ic9fb7b7711fad25620cff8890c2931d8632eac71
    Signed-off-by: Sudokamikaze <sudokamikaze@protonmail.com>

--- device.mk
+++ device.mk
@@ -245,7 +245,8 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
