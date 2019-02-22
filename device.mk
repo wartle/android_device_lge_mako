airend/android_device_lge_hammerhead

device/manifest: Make keymaster binderized always
Bug: 35764921
Test: Boot the device and check the output of lshal. The keymaster
      service must be running and should have two clients: vold and
      keystore

Change-Id: I62c78ec17541675027204aeacb9fe0aa1c9df1f0
Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

device.mk
@@ -190,6 +190,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
     keystore.msm8974
 
 PRODUCT_PACKAGES += \

wartle/android_device_lge_mako

Change-Id: Ia876034975bd64a9913351ca93b4642d91c30583

device.mk
@@ -143,7 +143,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Keystore
 PRODUCT_PACKAGES += \