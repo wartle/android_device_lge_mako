31a1cb251d5e35d8954cec6f373840f14ba96df9
voron00 <alexey12424323@gmail.com>
2019-03-16 09:18:34 +0300

    mako: Enable full dexpreopt
    Consumes about ~30 mb of system space, but greatly
    speeds up first boot.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,11 +89,12 @@ TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x02000000U
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Dexpreopt
+# Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
     endif
   endif
 endif
