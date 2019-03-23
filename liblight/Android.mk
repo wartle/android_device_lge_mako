9f23d94820662b9a8ac72a7b913d9b8fadce7d8c
voron00 <alexey12424323@gmail.com>
2019-03-23 12:42:29 +0300

    lights: build with -Wno-error

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -15,10 +15,13 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := lights.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE := lights.mako
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
+LOCAL_CFLAGS += -Wno-error
+
 include $(BUILD_SHARED_LIBRARY)
