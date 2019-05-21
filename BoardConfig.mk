8d9dfdfdc9798b5dcfc871385c58b471b2c46921
voron00 <alexey12424323@gmail.com>
2019-05-21 14:59:11 +0300

    Revert "mako: Enable full dexpreopt"
    This reverts commit 31a1cb251d5e35d8954cec6f373840f14ba96df9.
    
    We need more space for gapps now ...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,12 +85,11 @@ TARGET_USES_ION := true
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x02000000U
 
 # Dexpreopt
-# Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
   endif
 endif
